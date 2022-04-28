4b56d3c1c8595ff63a6aa35209825e594f00e223
Florian <florianpasquett@gmail.com>
2022-04-28 13:53:50 +0200

    Modification function et ajout touche Entrée pour valider

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 function send(){
       //console.log(createUrl());
-      var oReq = new XMLHttpRequest();
+      const oReq = new XMLHttpRequest();
       oReq.addEventListener("load", function() {
           document.getElementById("translated_text").value = JSON.parse(this.responseText).translations[0].text;
 
       });
-      oReq.open("POST", createUrl(), true);
+      oReq.open("POST", url(), true);
       oReq.send();
       
 }
 
-function createUrl() {
+const url = () => {
     return `https://api-free.deepl.com/v2/translate?auth_key=${document.getElementById("key").value}&text=${document.getElementById("text").value}&${(document.getElementById("source_lang").value == "") ? "" : document.getElementById("source_lang").value + "&"}target_lang=${document.getElementById("target_lang").value}`;
 }
 
 document.getElementById("translate-btn").addEventListener("click", send);
+
+window.addEventListener("keydown", function(event) {
+    if(event.key == "Enter")
+        send();
+})
